0528927d3cff62c2800f4b77a67290dd1b505b44
Renke Bijlsma <renkebijlsma@gmail.com>
2017-05-03 23:12:04 +0200

    initial commit to get things started

---
+++ src/components/Users.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="users container">
+    <h1 class="page-header">Users</h1>
+      <ul>
+        <li v-for="user in users">{{ user.name }}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  data () {
+    return {
+      users: []  
+    }
+  },
+  methods: {
+    fetchCustomers() {
+      this.$http.get('https://jsonplaceholder.typicode.com/users')
+        .then(function(response) {
+          this.users = response.body;
+        })
+    }
+  },
+  created: function(){
+    this.fetchCustomers();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
